ef465b21d38e86a03490da76290e11f2bd6245fa
Marycorreia12 <correia_nety@hotmail.com>
2020-04-07 18:00:31 +0100

    Testando Rest

---
+++ test/java/com/maryCorreia/cursoUml/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryCorreia.cursoUml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
